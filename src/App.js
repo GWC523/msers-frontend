b1ac914dc1b37255f097ae25436b77fbcb2aae12
gwynethWC <gwyneth@mytdevcorp.com>
2023-03-11 14:36:11 +0800

    Added navbar component and homepage page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+/** PAGES **/
+import Homepage from './Pages/Homepage';
+
 
 function App() {
+  //APP NAME
+  document.title = "MSERS";
+  document.body.style = "background: #FFFFFF;";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Homepage/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/Pages/Homepage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+
+//css
+import "./Homepage.css"
+
+function Homepage() {
+  return (
+    <div className='page'>
+        <Navbar title={"MSERS"}/>
+    </div>
+  )
+}
+
+export default Homepage
